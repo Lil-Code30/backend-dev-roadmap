886f7e3f3ea645149cc976ed18e1cf6a671b9125
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-04-29 16:42:35 -0400

    feat: my first java code

---
+++ 01-Foundation/java/MyFirstProject/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main (String[] args){
+        // This is my first Java Code
+        System.out.println("I Like Pizza");
+        System.out.println("I Like Pizza it is very good");
+        System.out.println("Buy me pizza");
+    }
+}
