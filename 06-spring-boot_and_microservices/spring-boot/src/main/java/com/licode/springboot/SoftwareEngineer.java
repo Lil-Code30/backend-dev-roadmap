aa4a0d3c903fb26a61d4691c1867728dee752c84
Ismael Loko <107557003+Lil-Code30@users.noreply.github.com>
2026-07-09 16:28:24 -0400

    feat: Add Spring Boot sample app
    Create a new Spring Boot project with Maven wrapper, a root hello-world endpoint, and a `/api/v1/software-engineers` REST controller returning sample engineer data.

---
+++ 06-spring-boot_and_microservices/spring-boot/src/main/java/com/licode/springboot/SoftwareEngineer.java
@@ -0,0 +1,54 @@
+package com.licode.springboot;
+
+import java.util.Objects;
+
+public class SoftwareEngineer {
+    private Integer id;
+    private String name;
+    private String techStack;
+
+    public SoftwareEngineer() {
+    }
+
+    public SoftwareEngineer(Integer id, String name, String techStack) {
+        this.name = name;
+        this.techStack = techStack;
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getTechStack() {
+        return techStack;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTechStack(String techStack) {
+        this.techStack = techStack;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        SoftwareEngineer that = (SoftwareEngineer) o;
+        return Objects.equals(id, that.id) && Objects.equals(name, that.name) && Objects.equals(techStack, that.techStack);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, techStack);
+    }
+}
---
+++ 06-spring-boot_and_microservices/spring-boot/src/main/java/com/licode/springboot/SoftwareEngineerController.java
@@ -0,0 +1,20 @@
+package com.licode.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/software-engineers")
+public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers() {
+        return List.of(
+                new SoftwareEngineer(1, "James", "js, node, react,"),
+                new SoftwareEngineer(2, "Stacy", "java, spring, spring boot")
+        );
+    }
+}
